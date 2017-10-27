ddfc9ed1e6225280872a155af891cb3e07dd9976
dingolfsson <dai5@hi.is>
2017-10-27 23:53:08 +0000

    Bank Project

--- Hogan/Bank_Account/include/BankAccount.h
+++ Hogan/Bank_Account/include/BankAccount.h
@@ -6,30 +6,19 @@
 
 using namespace std;
 
-class BankAccount
-{
+class BankAccount{
     public:
-
         BankAccount();
-
         void readData();
-
         friend ostream& operator << (ostream& out, const BankAccount& bankAccount);
-
         void update();
-
         bool withdraw(double withdraw);
-
         int getNumber();
-
-
     private:
-
         string customer_name;
         int customer_number;
         double balance;
         double intrests;
-
 };
 
-#endif // BANKACCOUNT_H
+#endif
